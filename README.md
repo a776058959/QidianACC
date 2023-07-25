a9e59e7c63d9d16738087b81b272457f98a700e8
a776058959 <58454818+a776058959@users.noreply.github.com>
2023-07-25 22:07:52 +0800

    更新

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
+
 # QidianACC
- 自动领取起点章节币/福利中心自动看广告
+## 修改自([qidian_AutoLookAD](https://github.com/pzx521521/qidian_AutoLookAD))的起点APP福利中心自动领取脚本
+
+感觉原作者只靠时间延迟来点击有点不稳定，索性多加了一些sleep和一些循环判断之类的。
+
+## 内容列表
+
+- [安装](#安装)
+- [使用说明](#使用说明)
+- [更新](#更新)
+- [示例](#示例)
+- [相关仓库](#相关仓库)
+- [维护者](#维护者)
+
+## 安装
+
+1.下载安装[Autoxjs](https://github.com/kkevsekk1/AutoX/)软件。
+2.下载这个main.js文件([点击下载之后解压出main.js](https://github.com/a776058959/qidian_AutoLookAD/archive/refs/heads/master.zip))。
+3.复制到手机任意目录(能找到就行)。
+4.打开Autoxjs，点击右上角加号，点击导入。
+5.找到并导入main.js(也可以改个自己看着方便的名字再导入)。
+
+## 使用说明
+
+1.运行main.js，如无自动跳过开屏广告软件，则需要手动点击跳过起点APP开屏广告。
+2.等待脚本领取完成。
+3.如有报错，请反馈。
+
+## 更新
+**2023.7.25更新**：部分延迟设置在更新之后出现问题，已经调整。然后每天第一次点开“我”页面时弹出的窗口能自动跳过了(暂未测试，明天测试有效果就再来删除这个括号内容)。
+
+**2023.6.4更新**：起点app奖励领取的控件改版，所以脚本里也跟着改了。顺便稍微改一改其他部分的逻辑
+
+## 示例
+
+[![9KTJ32BGAZ5XBHT7R84.md.png](https://cdnjson.com/images/2023/04/06/9KTJ32BGAZ5XBHT7R84.md.png)](https://cdnjson.com/image/jhksM)
+
+## 相关仓库
+
+- [qidian_AutoLookAD](https://github.com/pzx521521/qidian_AutoLookAD) — 基于[autoxjs](http://doc.autoxjs.com/)起点福利中心自动脚本。
+
+## 维护者
+
+[@a776058959](https://github.com/a776058959)。
---
+++ main.js
@@ -0,0 +1,215 @@
+importClass(android.content.Context);
+importClass(android.provider.Settings);
+try {
+    var enabledServices = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
+    log('当前已启用的辅助服务\n', enabledServices);
+    var Services = enabledServices + ":com.script.main/com.stardust.autojs.core.accessibility.AccessibilityService";
+    Settings.Secure.putString(context.getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES, Services);
+    Settings.Secure.putString(context.getContentResolver(), Settings.Secure.ACCESSIBILITY_ENABLED, '1');
+    toastLog("成功开启AutoJS的辅助服务");
+} catch (error) {
+    //授权方法：开启usb调试并使用adb工具连接手机，执行 adb shell pm grant com.script.main android.permission.WRITE_SECURE_SETTINGS
+    toastLog("\n请确保已给予 WRITE_SECURE_SETTINGS 权限\n\n授权代码已复制，请使用adb工具连接手机执行(重启不失效)\n\n", error);
+    setClip("adb shell pm grant com.script.main android.permission.WRITE_SECURE_SETTINGS");
+}
+
+
+function MyClick(str){
+    var meButton = undefined
+    for(let m = 1; m < 3; m++){
+        meButton = text(str).findOnce()
+        if (meButton != undefined){
+            meButton = meButton.parent();
+            toastLog("找到了" + str)
+            meButton.click();
+            break;
+        }else{
+            toastLog("未找到(" + str + ")按钮,5秒后重试")
+            sleep(5000)
+            m = 1
+        }
+    }
+    sleep(1000)
+    weButton = text("我知道了").findOnce()
+    if(weButton != undefined){
+        weButton.click()
+    }
+}
+
+function Close(){
+    var cButton =text("继续观看").findOnce()
+    if(cButton != undefined){
+        cButton.click()
+        sleep(500)
+    }
+
+    // back() 失效 原因未知
+    var meButton =className("android.widget.Image").text("cross").findOnce()
+    if (meButton == undefined){
+        sleep(1000)
+        meButton = className("android.widget.Image").text("此图片未加标签。打开右上角的“更多选项”菜单即可获取图片说明。").findOnce()
+    }
+    if(meButton != undefined){
+        meButton = meButton.parent();
+        toastLog("找到了关闭按钮")
+        meButton.click()
+    }else{
+        for(let c = 0; c < 5; c++){
+            sleep(1000)
+            meButton =className("android.widget.Image").text("cross").findOnce()
+            //其他判断暂时待定
+            if(meButton != undefined){
+                meButton = meButton.parent();
+                toastLog("找到了关闭按钮")
+                meButton.click()
+                break
+            }
+        }
+    }
+}
+
+function st(){
+    toastLog("全部奖励已领完,停止脚本")
+    engines.stopAll()
+}
+
+function lq(){
+    toastLog("查找我知道了")
+    sleep(500)
+    var cButton =text("我知道了").findOnce()
+    if (cButton != undefined){
+        cButton = cButton.parent();
+        cButton.click()
+        sleep(500)
+    }else{
+        sleep(2000)
+        cButton =text("我知道了").findOnce()
+        if(cButton != undefined){
+            cButton = cButton.parent();
+            cButton.click()
+            sleep(500)
+        }else{
+            //如果是第10/11个就没有领取按钮就正常
+            toastLog("未找到领取按钮")
+            
+        }
+    }
+}
+
+home()
+
+if (launchApp("起点读书")){
+    toastLog("启动起点读书成功")
+}else{
+    toastLog("启动起点读书失败")
+}
+sleep(3000)
+
+//找到我
+MyClick("我")
+sleep(2000)
+MyClick("福利中心")
+sleep(2000)
+
+//看视频1-8
+var fButton = undefined
+var eButton = undefined
+let b = 0;
+fButton = textMatches("看第\\d+个视频").findOnce();
+eButton = textMatches("看视频").findOnce();
+for(let a = 1; a < 3; a++){
+    sleep(2000)
+    fButton = textMatches("看第\\d+个视频").findOnce();
+    eButton = textMatches("看视频").findOnce();
+    a = 1
+    if(fButton != undefined){
+        see()
+        break
+    }else{
+        sleep(2000)
+    }
+    if(eButton == undefined){
+        b = b+1;
+        if(b == 5){
+            b = 4
+            fButton = textMatches("明天再来吧").findOnce();
+            eButton = textMatches("已领取").findOnce();
+            if(fButton != undefined && eButton != undefined){
+                st()
+            }
+        }
+    }else{
+        see2()
+        break
+    }
+}
+
+function see(){
+    for (var index = 1; index < 9; index++) {
+        sleep(1000)
+        var meButton = undefined
+        meButton =textMatches("看第" + index +"个视频").findOnce();
+        if (meButton == undefined){
+            sleep(1500)
+            meButton =textMatches("看第" + index +"个视频").findOnce();
+            if(meButton != undefined){
+                index = index-1
+                continue
+            }
+            toastLog(caption + "视频没有找到!,1s后查找下一个")
+            sleep(1000)
+            continue
+        }
+        let caption = meButton.text()
+        meButton.click()
+        toastLog(caption + "开始!等待20s") 
+        sleep(20000)
+        Close()  
+        sleep(2000)
+        lq()
+        toastLog(caption + ",结束!") 
+        sleep(2000)
+    }
+    eButton = textMatches("看视频").findOnce();
+    if(eButton != undefined){
+        see2()
+    }else{
+        st()
+    }
+}
+function see2(){
+    eeButton = textMatches("看视频").findOnce();
+    if(eeButton != undefined){
+        toastLog("尝试划动屏幕")
+        const btrue = new Boolean(true);
+        btrue = textMatches("明天再来吧").findOnce().scrollForward()
+        toastLog(btrue)
+    }
+    
+    var eButton = undefined
+    for(let a = 9; a <= 13; a++){
+        sleep(1000)
+        eButton = textMatches("看视频").findOnce();
+        if(eButton != undefined){
+            eButton.click()
+            sleep(500)
+            toastLog("看第" + a + "个视频等待20s")
+            sleep(20000)
+            Close()
+            sleep(1000)
+            lq()
+        }else{
+            eButton = textMatches("看视频").findOnce();
+            if(eButton != undefined){
+                eButton.click()
+                sleep(500)
+                toastLog("看第" + a + "个视频等待20s")
+                sleep(20000)
+                Close()
+                sleep(1000)
+                lq()
+            }
+        }
+    }
+    st()
+}
